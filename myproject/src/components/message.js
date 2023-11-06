12ad2537b87f5d6d83fe12eb453379ecd1b085d7
2sammy <samuelmanoah61@gmail.com>
2023-11-06 04:50:09 +0300

    functional and class component

---
+++ myproject/src/components/message.js
@@ -0,0 +1,10 @@
+import { Component} from 'react'
+
+// inherit from the component
+class Message extends Component {
+    render() {
+        return <h1>this is class component</h1>
+    }
+
+}
+export default Message
