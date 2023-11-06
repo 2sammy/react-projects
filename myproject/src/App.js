1d486d4dae9c5f350d950f8ae06f1fee17fbd99d
2sammy <samuelmanoah61@gmail.com>
2023-11-06 05:20:27 +0300

    props

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import Hello from './components/Hello';
-import Message from './components/message';
+//import Hello from './components/Hello';
+//import Message from './components/message';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
-      <Hello />
-      <Message />
+      <Profile name="mike"></Profile>
+      <h1>hello ,my profile picture</h1>
+
       
       </div>
   )
---
+++ myproject/src/components/Profile.js
@@ -0,0 +1,4 @@
+function Profile (props) {
+    return <h1>Name: {props.name} {props.lastname}
+    </h1>
+}
