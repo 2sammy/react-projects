f1a8fc4e7dba2f40753f6f4f6286c5a635299c9a
2sammy <samuelmanoah61@gmail.com>
2023-11-08 07:43:47 +0300

    states

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,16 +1,14 @@
 //import logo from './logo.svg';
 import './App.css';
 //import Hello from './components/Hello';
+import Counter from './components/Counter';
 import {Message, Content} from './components/message';
 import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
-      <Profile name="mike" />
-      <Message content="hello receive my content" />
-      <h1>hello ,my profile picture</h1>
-
+     <Counter />
       
       </div>
   )
---
+++ myproject/src/components/Counter.js
@@ -0,0 +1,30 @@
+import { Component } from "react";
+
+class Counter extends Component {
+    constructor(){
+        super();
+        this.state= {
+            counter: 100,
+        };
+    }
+    //increment(){
+       // this.setState ({
+           // counter: this.state.counter +1
+      //  });
+    //}
+    decrement(){
+        this.setState({
+            counter: this.state.counter -1,
+        });
+    }
+    render(){
+        return(
+            <> 
+        <h1>Count value is:{this.state.counter}</h1>
+        <button onClick={() => this.decrement()}>click the button</button>
+        </>
+        )
+
+    }
+}
+export default Counter;
